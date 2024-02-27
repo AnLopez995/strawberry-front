77e85bf4d5ddff4f596bc655516049a08e8938dd
= <=>
2024-02-26 22:35:46 -0500

    Archivos de layour

---
+++ src/app/body.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {}
+
+const Body: React.FC<Props> = () => {
+  // Aquí va tu lógica del componente
+
+  return (
+    <div>
+      <h2>Hola perro</h2>
+    </div>
+  );
+}
+
+export default Body;
