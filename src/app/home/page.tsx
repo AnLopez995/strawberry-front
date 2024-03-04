f846d0679de4fd9a149ec009bc338be765c2421d
Andres Lopez Cala <libardo.lopez@disfarma.com.co>
2024-03-04 17:46:43 -0500

    Actualizar estilos y agregar sección de productos en la página de inicio

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import Body from "./components/body";
 
 export default function HomePage({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen max-h-screen">
       <Body />
       {children}
     </div>
